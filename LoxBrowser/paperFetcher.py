368ee3f93c195fada22300e948e7879bf3d27e2a
Martijn Landman <martijn.landman@hotmail.nl>
2017-05-25 22:22:38 +0200

    paperFetcher slaat de opgehaalde papers op in een .txt

--- LoxBrowser/paperFetcher.py
+++ LoxBrowser/paperFetcher.py
@@ -5,34 +5,41 @@ Created on Thu May 25 13:26:02 2017
 @author: Martijn Landman
 """
 
-from Bio import Entrez
-from Bio import Medline
+from Bio import Entrez, Medline
+import datetime
+import os
 
 TERM = '(("1990"[Date - Publication] : "3000"[Date - Publication]) AND (LOX OR lipoxygenase)) NOT cancer'
 
-Entrez.email = 'A.N.Other@example.com'
-h = Entrez.esearch(db='pubmed', retmax='1', term=TERM)
-result = Entrez.read(h)
-resultCount = result['Count']
-print('Total number of publications containing lipoxygenase: {0}'.format(resultCount))
+def main():
+    paperCount = getPaperCount()
+    fetchPapers(paperCount)
+    with open(str(datetime.date.today())+".txt") as handle:
+        for record in Medline.parse(handle):
+            print(record["PMID"])
 
-print('Getting {0} publications containing lipoxygenase...'.format(resultCount))
-h = Entrez.esearch(db='pubmed', retmax=resultCount, term=TERM)
-result = Entrez.read(h)
-ids = result['IdList']
-h = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
-records = Medline.parse(h)
-records = list(records)
+def getPaperCount():
+    Entrez.email = 'A.N.Other@example.com'
+    net_handle = Entrez.esearch(db='pubmed', retmax='1', term=TERM)
+    result = Entrez.read(net_handle)
+    paperCount = result['Count']
+    print('Total number of publications containing lipoxygenase: {0}'.format(paperCount))
+    net_handle.close
+    return paperCount
 
-#authors = []
-for record in records:
-    print("title:", record.get("TI", "?"))
-    print("authors:", record.get("AU", "?"))
-    print("source:", record.get("SO", "?"))
-    print("")
-#    au = record.get('AU', '?')
-#    for a in au:
-#        if a not in authors:
-#            authors.append(a)
-#    authors.sort()
-#print('Authors: {0}'.format(', '.join(authors)))
+def fetchPapers(paperCount):
+    print('Getting {0} publications containing lipoxygenase...'.format(paperCount))
+    h = Entrez.esearch(db='pubmed', retmax=paperCount, term=TERM)
+    result = Entrez.read(h)
+    ids = result['IdList']
+    h.close
+    filename = "PubMedFetch_"+str(datetime.date.today())+".txt"
+    if not os.path.isfile(filename):
+        net_handle = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
+        output_handle = open(filename,"w")
+        output_handle.write(net_handle.read())
+        output_handle.close()
+        net_handle.close()
+        print("Saved all {0} publications".format(paperCount))
+
+main()
