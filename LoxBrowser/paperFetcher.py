e267d52c8bc2aa6b52ae7d905f2fc7338a338765
Martijn Landman <martijn.landman@hotmail.nl>
2017-05-27 15:17:34 +0200

    paperfetcher now checks for new publications

--- LoxBrowser/paperFetcher.py
+++ LoxBrowser/paperFetcher.py
@@ -6,17 +6,32 @@ Created on Thu May 25 13:26:02 2017
 """
 
 from Bio import Entrez, Medline
-import datetime
 import os
 
 TERM = '(("1990"[Date - Publication] : "3000"[Date - Publication]) AND (LOX OR lipoxygenase)) NOT cancer'
 
 def main():
-    paperCount = getPaperCount()
-    fetchPapers(paperCount)
-    with open("PubMedFetched\\PubMedFetch_"+str(datetime.date.today())+".txt") as handle:
+    if checkPubMedFetched():
+        updateCount = checkForUpdates()
+        if updateCount!=0:
+            print("New publications available...")
+            fetchPapers(updateCount,"a")
+        else:
+            print("There are no new publications available")
+    else:
+        fetchPapers(getPaperCount(),"w")
+
+def checkPubMedFetched():
+    return os.path.isfile("PubMedFetched\\PubMedFetched.txt")
+
+def checkForUpdates():
+    recordCount = 0
+    with open("PubMedFetched\\PubMedFetched.txt") as handle:
         for record in Medline.parse(handle):
-            print(record["PMID"])
+            recordCount += 1
+    paperCount = int(getPaperCount())
+    updateCount = paperCount-recordCount
+    return updateCount
 
 def getPaperCount():
     Entrez.email = 'A.N.Other@example.com'
@@ -27,20 +42,18 @@ def getPaperCount():
     net_handle.close
     return paperCount
 
-def fetchPapers(paperCount):
-    print('Getting {0} publications containing lipoxygenase...'.format(paperCount))
+def fetchPapers(paperCount, writeMode):
     h = Entrez.esearch(db='pubmed', retmax=paperCount, term=TERM)
     result = Entrez.read(h)
     ids = result['IdList']
     h.close
-    filename = "PubMedFetched\\PubMedFetch_"+str(datetime.date.today())+".txt"
-    if not os.path.isfile(filename):
-        os.makedirs("PubMedFetched", exist_ok=True)
-        net_handle = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
-        output_handle = open(filename,"w")
-        output_handle.write(net_handle.read())
-        output_handle.close()
-        net_handle.close()
-        print("Saved all {0} publications".format(paperCount))
+    print('Getting {0} publications containing lipoxygenase...'.format(paperCount))
+    os.makedirs("PubMedFetched", exist_ok=True)
+    net_handle = Entrez.efetch(db='pubmed', id=ids, rettype='medline', retmode='text')
+    output_handle = open("PubMedFetched\\PubMedFetched.txt", writeMode)
+    output_handle.write(net_handle.read())
+    output_handle.close()
+    net_handle.close()
+    print("Saved all {0} publications".format(paperCount))
 
 main()
