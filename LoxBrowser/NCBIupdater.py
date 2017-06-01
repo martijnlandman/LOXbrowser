b8d831f45b6f2c633ff30b8d3a15ce5a32212839
Martijn Landman <martijn.landman@hotmail.nl>
2017-06-01 13:08:45 +0200

    added textmining pre-process

--- LoxBrowser/NCBIupdater.py
+++ LoxBrowser/NCBIupdater.py
@@ -6,6 +6,7 @@ Created on Sat May 27 17:28:54 2017
 """
 
 from Bio import Medline
+import nltk
 
 def main():
     try:
@@ -15,6 +16,8 @@ def main():
                 count += 1
                 print("Record {0}".format(count))
                 recordHandler(record)
+                if count == 1:
+                    break
     except FileNotFoundError:
         print("Can't find PubMedFetched.txt in the PubMedFetched directory")
 
@@ -25,9 +28,20 @@ def recordHandler(record):
         record_title = record["TI"]
         record_author = record["AU"]
         record_publicationDate = record["DP"]
-        print("PMID: {0}\nURL: {1}\nTitle: {2}\nAuthor: {3}\nDP: {4}\n".format(record_PubMedID,record_URL,record_title,record_author,record_publicationDate))
+        record_abstract = textMiner(record["AB"])
+        print("PMID: {0}\nURL: {1}\nTitle: {2}\nAuthor: {3}\nDP: {4}\nAB: {5}\n".format(record_PubMedID,record_URL,record_title,record_author,record_publicationDate,record_abstract))
     except KeyError as err:
         print("Received error from server %s" % err)
         print("Disrecarding record for lacking information")
+
+def textMiner(abstract):
+    sentences = ie_preprocess(abstract)
+    return sentences
+
+def ie_preprocess(abstract):
+    sentences = nltk.sent_tokenize(abstract)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
 
 main()
