3d4bc24ed2d7b0ff948f7c9a2b01f1cf2d60d310
martijnlandman <martijn.landman@hotmail.nl>
2017-06-12 14:35:39 +0200

    added database functionality to NCBIupdater

--- LoxBrowser/NCBIupdater.py
+++ LoxBrowser/NCBIupdater.py
@@ -5,14 +5,14 @@ Created on Sat May 27 17:28:54 2017
 @author: Martlan Landman
 """
 
-import mysql.connector
 from Bio import Medline
-import nltk, re, mysql.connector
+import nltk, mysql.connector
 
 def main():
     updateKeywords()
     print("Done updating keywords")
     recordParser()
+    print("Done saving records")
 
 def updateKeywords():
     try:
@@ -43,6 +43,7 @@ def recordParser():
                 count += 1
                 print("Record {0}".format(count))
                 results = recordHandler(record)
+#                resultsToDB(results)
                 if count == 10:
                     break
     except FileNotFoundError:
@@ -56,8 +57,8 @@ def recordHandler(record):
         results['title'] = record["TI"]
         results['author'] = record["AU"]
         results['publicationDate'] = record["DP"][0:4]
-        results['abstract'] = textMiner(record["AB"], record["TI"])
-        print("PMID: {0}\nURL: {1}\nTitle: {2}\nAuthor: {3}\nDP: {4}\nAB: {5}\n".format(results['PubMedID'],results['URL'],results['title'],results['author'],results['publicationDate'],results['abstract']))
+        results['keywords'] = textMiner(record["AB"], record["TI"])
+        print("PMID: {0}\nURL: {1}\nTitle: {2}\nAuthor: {3}\nDP: {4}\nAB: {5}\n".format(results['PubMedID'],results['URL'],results['title'],results['author'],results['publicationDate'],results['keywords']))
         return results
     except KeyError as err:
         print("Received error from server %s" % err)
@@ -66,13 +67,12 @@ def recordHandler(record):
 def textMiner(abstract, title):
 #    sentences = nltk.sent_tokenize(abstract)
     keywords = []
-    words = nltk.word_tokenize(abstract)
-    print("words: {0}".format(words))
+    words = nltk.word_tokenize(abstract) + nltk.word_tokenize(title)
     with open("BioCorpus.txt") as file_handle:
         for keyword in file_handle:
             keyword = keyword.strip()
             if keyword in words:
-                keywords.append(keyword)
+                keywords.append(keyword.lower())
 #    sentences = [nltk.pos_tag(sent) for sent in sentences]
 #    NN_tags = []
 #    for sentence in sentences:
@@ -80,35 +80,36 @@ def textMiner(abstract, title):
 #            match = re.match(r'(NN(.*))|(JJ.?)', word[1])
 #            if match and word not in NN_tags:
 #                NN_tags.append(word)
-    return keywords
+    return sorted(list(set(keywords)))
 
-#def saveResults(results):
-#    conn = mysql.connector.connect(host = "127.0.0.1",
-#                         user = "owe8_1617_gr3",
-#                         password = "blaat1234",
-#                         db="owe8_1617_gr3")
-#    cursor = conn.cursor ()
-#
-#    cursor.execute ("INSERT INTO results(`pubMed ID`,url,Title) VALUES (%i,%s,%s)", (results['PubMedId'], results['URL'], results['title']));
-#    cursor.execute ("INSERT INTO Author(idAuthor,initials,`Last name`) VALUES (%s,%s,%s)", (Author_ID, Initials, Last_name));
-#    cursor.execute ("INSERT INTO results_has_Author(`results_pubMed ID`,Author_idAuthor) VALUES (%s,%s)", (results['PubMedId'], Author_ID));
-#    
-#    cursor.execute ("INSERT INTO Lox(`lox`,`NCBI prot_ID`,`fullname`) VALUES (%s,%s,%s)", (Lox, NCBI_lox, Lox_full));
-#    cursor.execute ("INSERT INTO results_has_Lox(`results_pubMed ID`,Lox_lox) VALUES (%s,%s)", (results['PubMedId'], Lox));
-#     
-#    cursor.execute ("INSERT INTO Organism(`Name`,`Kingdom`) VALUES (%s, %s)", (Organism_Name, Kingdom));
-#    cursor.execute ("INSERT INTO results_has_Organism(`results_pubmed ID`,`Organism_Name`) VALUES (%s,%s)", (results['PubMedId'], Organism_name));
-#     
-#    cursor.execute ("INSERT INTO `publication_date`(`date`) VALUES (%s)", (results['publicationDate']));
-#    cursor.execute ("INSERT INTO `results_has_publication_date`(`results_pubMed ID`,`publication_date_date`) VALUES (%s,%s)", (results['PubMedId'], results['publicationDate']));
-#     
-#    cursor.execute ("INSERT INTO `keywords`(`keyword_ID`,`keyword`) VALUES (%d,%s)", (keyword_ID, keyword));
-#      
-#    cursor.execute ("INSERT INTO `Toepassing`(`Toepassing`) VALUES (%s)", (Toepassing));
-#    cursor.execute ("INSERT INTO `results_has_Toepassing`(`results_pubMed ID`,`Toepassing_Toepassing`) VALUES (%s,%s))", (results['PubMedId'],Toepassing));
-# 
-#    conn.commit()
-#
-#    rows = cursor.fetchall ()
+def connectToDB():
+    conn = mysql.connector.connect(host = "127.0.0.1",
+                         user = "owe8_1617_gr3",
+                         password = "blaat1234",
+                         db="owe8_1617_gr3")
+    return conn
+
+def resultsToDB(results):
+    conn = connectToDB()
+    cursor = conn.cursor()
+    
+    cursor.execute ("INSERT IGNORE INTO results(`pubMed ID`,url,Title) VALUES (%i,%s,%s)", (results['PubMedID'], results['URL'], results['title']))
+    for author in results['author']:
+        author_id = hash(author)
+        author = author.split(" ")
+        cursor.execute ("INSERT IGNORE INTO Author(idAuthor,initials,`Last name`) VALUES (%s,%s,%s)", (author_id, author[1], author[0]))
+        cursor.execute ("INSERT IGNORE INTO results_has_Author(`results_pubMed ID`,Author_idAuthor) VALUES (%i,%s)", (results['PubMedID'], author_id))
+    
+    cursor.execute ("INSERT IGNORE INTO publication_date(`date`) VALUES (%s)", (results['publicationDate']))
+    cursor.execute ("INSERT IGNORE INTO results_has_publication_date(`results_pubMed ID`,publication_date_date) VALUES (%s,%s)", (results['PubMedID'], results['publicationDate']))
+    for keyword in results['keywords']:
+        
+        cursor.execute ("INSERT IGNORE INTO keywords(keyword) VALUES (%s)", (keyword))
+        keyword_ID = cursor.insert_id()
+        cursor.execute ("INSERT IGNORE INTO results_has_keywords (results_pubMed_ID, keywords_keyword_ID) VALUES (%i, %s )", (results['PubMedID'], keyword_ID))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 main()
